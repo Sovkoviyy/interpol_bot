f5ce85e9d2c36ffd55fa527b6897fb1b1fa580c9
Sovkoviyy <sovkoviy@users.noreply.github.com>
2026-09-22 21:18:44 +0300

    fix(launcher): enforce CRLF line endings and simplify start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,5 @@
 @echo off
 chcp 65001 > nul
-title INTERPOL BOT • Universal Launcher
 cd /d "%~dp0"
 node start.js %*
-if errorlevel 1 (
-    echo.
-    echo [ОШИБКА] Произошел сбой при запуске бота.
-    pause
-)
+pause
